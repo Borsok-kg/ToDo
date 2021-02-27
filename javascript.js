0f7a19f5bbc1f76780ad76479893ee29b9fd098b
Tagaev Azat <braza@yandex.ru>
2021-02-27 23:25:16 +0600

    ToDo

---
+++ javascript.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const todoControl = document.querySelector('.todo-control'),
+      headerInput = document.querySelector('.header-input'),
+      todoList = document.querySelector('.todo-list'),
+      todoCompleted = document.querySelector('.todo-completed');
+
+let todoData = [];
+
+
+
+let jsonStringify = function() {
+    let jsonRecord = JSON.stringify(todoData);
+    localStorage.toDo = jsonRecord;
+};
+
+let jsonParse = function() {
+    let jsonParse = localStorage.toDo;
+    todoData = JSON.parse(jsonParse);
+    return todoData;
+};
+
+const render = function() {
+    // Очищаем элементы
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+    // Перебираем и добавляем элементы
+    todoData.forEach(function(item, i) {
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+            '<div class="todo-buttons">' +
+                '<button class="todo-remove"></button>' +
+                '<button class="todo-complete"></button>' +
+            '</div>';
+        // Проверка элемента на - выполнил или нет
+        if (item.complete) {
+            todoCompleted.append(li);
+            jsonStringify();
+        } else {
+            todoList.append(li);
+        }
+        // Перекидываем элемент в выполненые
+        const btnComplete = li.querySelector('.todo-complete');
+        btnComplete.addEventListener('click', function() {
+            item.complete = !item.complete;
+            render();
+            jsonStringify();
+        });
+        // Удаляем элемент
+        const btnRemove = li.querySelector('.todo-remove');
+        btnRemove.addEventListener('click', function() {
+            todoData.splice(i, 1);
+            jsonStringify();
+            jsonParse();
+            render();
+        });
+    });
+    // Очищаем инпут
+    headerInput.value = '';
+};
+
+todoControl.addEventListener('submit', function(event) {
+    if (headerInput.value === '') {
+        alert('Необходимо написать план!');
+    } else {
+        event.preventDefault();
+        const newTodo = {
+            value: headerInput.value,
+        };
+        todoData.push(newTodo);
+        jsonStringify();
+        render();
+    }
+});
+
+jsonParse();
+render();
